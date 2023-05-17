774d85d27674689baab2a8124d240c5284701253
dzhovtobryukh <10772794+marcupan@users.noreply.github.com>
2023-05-17 14:17:30 +0300

    fix router
     - remove deleted example import

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { postsRouter } from "~/server/api/routers/posts";
 
 /**
@@ -8,7 +7,6 @@ import { postsRouter } from "~/server/api/routers/posts";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   posts: postsRouter,
 });
 
